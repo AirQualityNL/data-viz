274311067daea8d8ade1ce039fe5938209acee0a
Kerimcan Güney <464639@student.fontys.nl>
2024-05-16 12:32:48 +0200

    removed style attribute ( WE USE TAILWIND)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Home() {
   return (
-    <div>
-      <MapContainer
-        center={[51.4416, 5.4697]}
-        zoom={12}
-        style={{ height: "400px", width: "100%" }}
-      >
+    <div className="h-screen">
+      <MapContainer className="h-full" center={[51.4416, 5.4697]} zoom={12}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
