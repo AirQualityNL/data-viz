3a75a37889a56d81ab40234849e13d77fb448d46
Kerimcan Güney <464639@student.fontys.nl>
2024-05-16 13:48:57 +0200

    Basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,25 @@ import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <MapContainer className="h-full" center={[51.4416, 5.4697]} zoom={12}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </MapContainer>
+    <div className="w-full h-screen grid-flow-row">
+      <div className="h-16 flex justify-center items-center">
+        <h1 className="text-2xl font-bold text-center">Title place holder</h1>
+      </div>
+      <div className="flex justify-center w-full h-4/6">
+        <MapContainer
+          className="w-full h-full"
+          center={[51.4416, 5.4697]}
+          zoom={12}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+      </div>
+      <div className="flex justify-center items-center">
+        <p className="text-sm">Some small text at the bottom explaining</p>
+      </div>
     </div>
   );
 }
