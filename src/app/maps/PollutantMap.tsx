1850818baf2b0d4eb4c3c08b813c1f5b87aa5024
Ryan Bours <ryan.bours@gmail.com>
2024-05-19 14:37:54 +0200

    fixed duplicate interface

--- src/app/maps/PollutantMap.tsx
+++ src/app/maps/PollutantMap.tsx
@@ -1,33 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Marker, Popup, useMap } from "react-leaflet";
-import { PollutantData } from "../interfaces/pollutant";
+import { Pollutant } from "@/app/interfaces/pollutants"
 import pollutants from "../../data/pollutants.json";
 
-// Define interface for pollutant data
-interface PollutantData {
-  Station: string;
-  Latitude: number;
-  Longitude: number;
-  PM1: number;
-  PM25: number;
-  PM10: number;
-  NO2: number;
+interface Props {
+  displayPM1: boolean;
+  displayPM25: boolean;
+  displayPM10: boolean;
+  displayNO2: boolean;
 }
 
-interface Props {
-    displayPM1: boolean;
-    displayPM25: boolean;
-    displayPM10: boolean;
-    displayNO2: boolean;
-  }
-
 export const PollutantMap: React.FC<Props> = ({
-    displayPM1,
-    displayPM25,
-    displayPM10,
-    displayNO2,
-  }) => {
-  const [pollutantData, setPollutantData] = useState<PollutantData[]>([]);
+  displayPM1,
+  displayPM25,
+  displayPM10,
+  displayNO2,
+}) => {
+  const [pollutantData, setPollutantData] = useState<Pollutant[]>([]);
   const map = useMap();
 
   useEffect(() => {
